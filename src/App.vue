4bb7900674f79531cb81bc5fc9a09f66d5291319
mrandrew1 <47503129+mrandrew1@users.noreply.github.com>
2020-10-07 14:52:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <Navbar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="nav">
+    <!-- <div class="leftmenu"> -->
+    <li>
+      <img src="https://vuejs.org/images/usave.png" alt="Logo" width="90px" />
+    </li>
+    <li><router-link to="/">Home </router-link></li>
+    <li><router-link to="/about"> About&nbspus </router-link></li>
+    <li><router-link to="/projects"> projects </router-link></li>
+    <li class="prelast"></li>
+    <li><router-link to="/contacts"> contacts </router-link></li>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.nav {
+  /*генерирует flex-контейнер уровня блока*/
+  display: -webkit-flex;
+  display: flex;
+  background-color: cadetblue;
+  color: rgba(133, 216, 78, 0.918);
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 96%;
+  margin: auto;
+  border-radius: 45px;
+  font-weight: 400;
+  font-size: 1em;
+}
+.nav .prelast {
+  flex-grow: 1;
+  min-width: 0px;
+}
+.leftmenu {
+  line-height: 1.3em;
+  display: flex;
+}
+.rightmenu {
+  line-height: 20pt;
+  display: flex;
+}
+.nav li {
+  margin: 1.2em;
+  list-style-type: none;
+}
+
+.nav li :hover {
+  border-bottom: 2px solid white;
+}
+
+@media screen and (max-width: 620px) {
+  .nav {
+    justify-content: center;
+  }
+  .prelast {
+    display: none;
+  }
+  .nav li {
+    width: 100%;
+    margin: 0.3em;
+  }
+}
+</style>
